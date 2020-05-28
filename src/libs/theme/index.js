c12fe2ad7b802aff8e222588b9869eec7d0fcab1
Thong Nguyen <thong.nguyencao.vn@gmail.com>
2020-05-28 22:48:19 +0700

    Implement theme switch

--- src/libs/theme/index.js
+++ src/libs/theme/index.js
@@ -1,2 +1,2 @@
 export {createTheme, getThemeValue} from './helpers';
-export {default as ThemeProvider} from './ThemeProvider';
+export {default as ThemeProvider, ThemeContext} from './ThemeProvider';
